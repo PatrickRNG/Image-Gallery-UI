f65db6f58585c4c3f56effc8afa0e6bb3dd0f1d8
Patrick Passarella <patrickpassarella@hotmail.com>
2018-01-23 10:23:03 -0200

    Updated FadeIn

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,31 @@
-const [current, imgs] = [
-    document.querySelector('#current'),
-    document.querySelectorAll('.images img')
-];
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.images img');
+const opacity = 0.6;
 
-imgs.forEach(img => img.addEventListener('click', e => current.src = e.target.src));
+imgs.forEach(img => img.addEventListener('click', imgClick));
 
 //imgs.forEach(function(img) {
 //   img.addEventListener('click', function(e) {
 //       current.src = e.target.src;
 //   });
 //});
+
+// Set first-image opacity
+imgs[0].style.opacity = opacity;
+
+function imgClick(e) {
+    // Reset the opacity
+    imgs.forEach(img => (img.style.opacity = 1));
+    
+    // Change current image to src of clicked image
+    current.src = e.target.src;
+    
+    // Add faceIn class
+    current.classList.add('fade-in');
+    
+    // Remove fade-in class after .2s
+    setTimeout(() => current.classList.remove('fade-in'), 500);
+    
+    // Change opacity to var
+    e.target.style.opacity = opacity;
+}
