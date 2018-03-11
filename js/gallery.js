f70b1acc8f9cf2442b9c539ba637eea4d46e853d
Patrick Passarella <patrickpassarella@hotmail.com>
2018-03-10 23:20:19 -0300

    Added a zoom when clicking main image and upgraded the UI

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,14 @@
 const current = document.querySelector('#current');
 const imgs = document.querySelectorAll('.images img');
 const opacity = 0.6;
+const main = document.querySelector('.main-img');
+const imageWrapper = document.querySelector('.images');
+const bgi = document.querySelector('#bgi');
 
 imgs.forEach(img => img.addEventListener('click', imgClick));
+window.addEventListener('click', fullImage);
 
-//imgs.forEach(function(img) {
-//   img.addEventListener('click', function(e) {
-//       current.src = e.target.src;
-//   });
-//});
+let images = Array.from(imgs).filter(a => a)
 
 // Set first-image opacity
 imgs[0].style.opacity = opacity;
@@ -28,4 +28,27 @@ function imgClick(e) {
     
     // Change opacity to var
     e.target.style.opacity = opacity;
+
+    bgi.style.backgroundImage = "url("+ current.src +")";    
+
+}
+
+function fullImage(e) {
+
+    console.log(e.target);
+
+    if (e.target == current) {
+        // images.forEach(img => img.style.display = 'none');
+        main.style.transform = 'scale(1.5, 1.5)';
+        main.style.transform += 'translateY(15%)';
+        imageWrapper.style.marginTop = '35%';
+        
+    }
+
+    if (e.target != current) {
+        images.forEach(img => img.style.display = 'block');        
+        main.style.transform = 'unset';
+        imageWrapper.style.marginTop = 'inherit';
+    }
+
 }
